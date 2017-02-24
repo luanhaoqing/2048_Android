999e2235483661491a908852acdd406cae48729f
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-24 15:03:01 -0500

    added a list of choices

--- app/src/main/java/com/example/hluan/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/hluan/mysecondapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     Button mybutton;
@@ -18,9 +19,14 @@ public class MainActivity extends AppCompatActivity {
         {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,GamePage.class);
-                startActivity(i);
-            }
+
+                Spinner choices=(Spinner) findViewById(R.id.choice);
+                String choice=String.valueOf(choices.getSelectedItem());
+                if(choice.equals("2048")) {
+                    Intent i = new Intent(MainActivity.this, GamePage.class);
+                    startActivity(i);
+
+                }    }
         });
     }
 }
